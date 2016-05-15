ff8eb1deb8eb7ec37709a33705a1970385b93285
dimi7ri <vasiliev.dimi@gmail.com>
2016-05-15 23:41:15 +1000

    Final

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,5 +1,25 @@
+Template.Recipe.onCreated(function(){
+	this.editMode = new ReactiveVar(false);
+});
+
+Template.Recipe.helpers({
+	updateRecipeId : function(){
+		return this._id;
+	},
+	editMode: function() {
+		return Template.instance().editMode.get();
+	}
+});
+
 Template.Recipe.events({
 	'click .toggle-menu': function() {
 		Meteor.call('toggleMenuItem', this._id, this.inMenu);
+	},
+	'click .fa-trash' : function(){
+		Meteor.call('deleteRecipe', this._id);
+	},
+	'click .fa-pencil' : function(event, template){
+		template.editMode.set(!template.editMode.get());
 	}
 });
+
--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -11,3 +11,9 @@ Template.Recipes.helpers({
 		return Recipes.find({});
 	}
 });
+
+Template.Recipes.events({
+ 	'click .new-recipe' : () =>{
+ 		Session.set('newRecipe', true);
+ 	}
+});
